f999be105b7c29da7d73bdf58c881e45a98cfacd
Marco Reis <marco.reis@tjdft.jus.br>
2013-10-09 11:24:00 -0300

    update

--- src/main/java/net/marcoreis/ecommerce/entidades/Categoria.java
+++ src/main/java/net/marcoreis/ecommerce/entidades/Categoria.java
@@ -1,8 +1,29 @@
 package net.marcoreis.ecommerce.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Categoria {
+    private Long id;
+    private String nome;
 
+    @Id
+    @GeneratedValue
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+
+    public String getNome() {
+	return nome;
+    }
 }
