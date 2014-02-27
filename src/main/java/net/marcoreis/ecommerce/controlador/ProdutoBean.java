1c4ac417bb02c03808e8b9d9ac35b165d7ffa33a
Marco Reis <marco.reis@tjdft.jus.br>
2014-02-27 19:14:33 -0300

    update

--- src/main/java/net/marcoreis/ecommerce/controlador/ProdutoBean.java
+++ src/main/java/net/marcoreis/ecommerce/controlador/ProdutoBean.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.event.ValueChangeEvent;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import net.marcoreis.ecommerce.entidades.Categoria;
 import net.marcoreis.ecommerce.entidades.Produto;
@@ -28,63 +30,94 @@ public class ProdutoBean extends BaseBean {
 
     @PostConstruct
     public void init() {
-	produto = new Produto();
-	produto.setCategoria(new Categoria());
-	carregarProdutos();
+        produto = new Produto();
+        produto.setCategoria(new Categoria());
+        carregarProdutos();
     }
 
     public void carregarProdutos() {
-	EntityManager em = JPAUtil.getInstance().getEntityManager();
-	produtos = em.createQuery("from Produto").getResultList();
-	em.close();
+        EntityManager em = JPAUtil.getInstance().getEntityManager();
+        produtos = em.createQuery("from Produto").getResultList();
+        em.close();
     }
 
     public void setProduto(Produto produto) {
-	this.produto = produto;
+        this.produto = produto;
     }
 
     public Produto getProduto() {
-	return produto;
+        return produto;
     }
 
     public Collection<Produto> getProdutos() {
-	return produtos;
+        return produtos;
     }
 
     public void salvar() {
-	try {
-	    EntityManager em = JPAUtil.getInstance().getEntityManager();
-	    em.getTransaction().begin();
-	    if (getEspecificacaoFabricante() != null) {
-		byte[] dados = IOUtils.toByteArray(getEspecificacaoFabricante()
-			.getInputstream());
-		getProduto().setEspecificacaoFabricante(dados);
-	    }
-	    em.persist(getProduto());
-	    em.getTransaction().commit();
-	    em.close();
-	    //
-	    getIndexador().indexarProdutos();
-	    getIndexador().fechar();
-	    //
-	    infoMsg("Dados gravados com sucesso");
-	} catch (Exception e) {
-	    errorMsg(e);
-	}
+        try {
+            EntityManager em = JPAUtil.getInstance().getEntityManager();
+            em.getTransaction().begin();
+            if (getEspecificacaoFabricante() != null) {
+                byte[] dados = IOUtils.toByteArray(getEspecificacaoFabricante()
+                        .getInputstream());
+                getProduto().setEspecificacaoFabricante(dados);
+            }
+            produto = em.merge(getProduto());
+            em.persist(getProduto());
+            em.getTransaction().commit();
+            em.close();
+            //
+            infoMsg("Dados gravados com sucesso");
+        } catch (Exception e) {
+            errorMsg(e);
+        }
     }
 
     public void setEspecificacaoFabricante(UploadedFile especificacaoFabricante) {
-	this.especificacaoFabricante = especificacaoFabricante;
+        this.especificacaoFabricante = especificacaoFabricante;
     }
 
     public UploadedFile getEspecificacaoFabricante() {
-	return especificacaoFabricante;
+        return especificacaoFabricante;
     }
 
     public IndexadorECommerce getIndexador() {
-	if (indexador == null) {
-	    indexador = new IndexadorECommerce();
-	}
-	return indexador;
+        if (indexador == null) {
+            indexador = new IndexadorECommerce();
+        }
+        return indexador;
     }
+
+    public String editar(Produto produto) {
+        this.produto = produto;
+        return "produto";
+    }
+
+    public void excluir(Produto produto) {
+        EntityManager em = JPAUtil.getInstance().getEntityManager();
+        try {
+            em.getTransaction().begin();
+            produto = em.merge(produto);
+            em.remove(produto);
+            em.getTransaction().commit();
+            produtos = em.createQuery("from Produto").getResultList();
+            infoMsg("Produdo excluído: " + produto.getNome());
+        } catch (Exception e) {
+            errorMsg(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void carregarProdutosPorCategoria(ValueChangeEvent evento) {
+        EntityManager em = JPAUtil.getInstance().getEntityManager();
+        String hql = "from Produto where categoria.id = :idCategoria";
+        Query query = em.createQuery(hql);
+        String newValue = evento.getNewValue().toString();
+        Long idCategoria = Long.parseLong(newValue);
+        query.setParameter("idCategoria", idCategoria);
+        produtos = query.getResultList();
+        em.close();
+    }
+
 }
---
+++ src/main/java/net/marcoreis/ecommerce/util/CategoriaConverter.java
@@ -0,0 +1,21 @@
+package net.marcoreis.ecommerce.util;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+//@FacesConverter
+public class CategoriaConverter implements Converter {
+
+    public Object getAsObject(FacesContext context, UIComponent component,
+            String value) {
+        return null;
+    }
+
+    public String getAsString(FacesContext context, UIComponent component,
+            Object value) {
+        return null;
+    }
+
+}
